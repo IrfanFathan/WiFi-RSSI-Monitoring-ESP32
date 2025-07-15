f1d58c7787886ca2ac629fb2df115edae952b14d
IrfanFathan <muhammadirfanvnb@gmail.com>
2025-07-15 21:49:34 +0530

    Stop tracking config.h

--- code/config.h
+++
@@ -1,4 +0,0 @@
-#define WiFi_SSID "STARVISION WIfi"  // Wifi SSID
-#define WiFi_PASSWORD "0plmnko9"      // Wifi password
-#define ThingSpeak_SERVER "http://api.thingspeak.com/update"  // ThingSpeak server URL
-#define ThingSpeak_API_KEY "6BL5FO7MGKR32RV5"  // ThingSpeak API key
